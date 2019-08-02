ea13975157c52fbcba207a09de46c5874c33346d
J.A. van der Valk <47637490+capitao-red-beard@users.noreply.github.com>
2019-08-02 13:26:28 +0200

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-
-r = requests.get(r'https://comtrade.un.org/api/get?r=40&token=K2fLb7Sc8KBGradNiBkbXH5J%2FZqjWgw7rOTFh30CuSLTk6EIDBUgFzMDBTXt%2FdQ0ZrREdP757T7Vt%2BfoFSNyOCmjua2zbq8A3hIydgZ8KG3UCX8H4DNArADwWikBDPPFBVkWJ%2FZayuGLjCZIydp7MLxcTZbB41pecp%2F%2Bcm%2FpEQc%3D')
-
-print(r.content)
